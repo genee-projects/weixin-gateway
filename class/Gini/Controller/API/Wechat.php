031b18c27a086bd21dbf2259364e15a8b7fd73f0
Jia Huang <iamfat@gmail.com>
2016-06-01 17:49:21 +0800

    add wechat api

--- class/Gini/Controller/API/Wechat.php
+++ class/Gini/Controller/API/Wechat.php
@@ -6,8 +6,47 @@ use \Gini\Controller\API;
 
 class Wechat extends API {
 
+    public function actionAuthorize($clientId, $clientSecret) {
+        $clients = (array) \Gini\Config::get('app.clients');
+        if (isset($clients[$clientId]) && $clients[$clientId] == $clientSecret) {
+            $_SESSION['app.client_id'] = $clientId;
+            return session_id();
+        }
+        return false;
+    }
+
+    private function isAuthorized() {
+        return isset($_SESSION['app.client_id']);
+    }
+
     public function actionGetUnionId($token) {
         return \Gini\Cache::of('wechat')->get('unionid['.$token.']');
+    }
+
+    public function actionGetAccessToken() {
+        if (!$this->isAuthorized()) return false;
+
+        $conf = \Gini\Config::get('wechat');
+        $app = new \Wechat\App($conf['app_id'], $conf['app_secret']);
+        return $app->getAccessToken();
+    }
+
+    public function actionGetTicket($type) {
+        if (!$this->isAuthorized()) return false;
+
+        $conf = \Gini\Config::get('wechat');
+        $app = new \Wechat\App($conf['app_id'], $conf['app_secret']);
+        $js = new \Wechat\JS($app);
+        return $js->getTicket($type);
+    }
+
+    public function actionGetJSSignPackage($url) {
+        if (!$this->isAuthorized()) return false;
+
+        $conf = \Gini\Config::get('wechat');
+        $app = new \Wechat\App($conf['app_id'], $conf['app_secret']);
+        $js = new \Wechat\JS($app);
+        return $js->getSignPackage($url);
     }
 
 }
